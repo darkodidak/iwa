1aaac239e3d68fb751fa36d0a8d89bb11c6a69f0
Darko <darkodidak1@gmail.com>
2021-07-02 20:03:42 +0200

    header refactor

--- header.php
+++ header.php
@@ -1,29 +1,35 @@
-</div>
 <nav>
-		<?php if(isset($_SESSION["id"])){ ?>
-				<li><a href="index.php">Početna stranica</a></li>
-				<li><a href="filtriranje.php">Filtriraj</a></li>
-				<li><a href="stat.php">Statistika</a></li>
-				<?php if(isset($_SESSION["tip"]) && ($_SESSION["tip"]) == 1) { ?>
-				<li><a href="dodavanje_zivotinje.php">Dodaj novu životinju</a></li>
-				<li><a href="moderator_stranica.php">Moderator stranica</a></li>
-				<?php } ?>
-				
-		<?php } ?>
-		
-		<?php if(isset($_SESSION["tip"]) && $_SESSION["tip"] == 0) { ?>
-		
-				<li><a href="dodavanje_zivotinje.php">Dodaj novu životinju</a></li>
-				<li><a href="korisnici.php">Korisnici</a></li>
-				<li><a href="registracija.php">Registracija</a></li>
-		<?php } ?>
+	<div class="row">
 		<?php if(!isset($_SESSION["id"])){ ?>
-		<div id="meni_prijava">
-			<li><a href="prijava.php">Prijava</a></li>
-		<?php } else { ?>
-			<li><a href="prijava.php?odjava=1">Odjava</a></li>
-		</div>
-		<?php } ?>
-	
+					<li><a href="prijava.php">Prijava</a></li>
+					<?php } else { ?>
+					<li><a href="prijava.php?odjava=1">Odjava</a></li>
+				<?php } ?>
+		<?php if(isset($_SESSION["id"])){ ?>
+	</div>
+	<div class="row">
+		<table>
+			<tr>
+				<th class="column1666 navigation-button"><a href="index.php">Početna stranica</a></th>
+				<th class="column1666 navigation-button"><a href="filtriranje.php">Filtriraj</a></th>
+				<th class="column1666 navigation-button"><a href="stat.php">Statistika</a></th>
+				<th>
+					<?php 
+					if(isset($_SESSION["tip"]) && ($_SESSION["tip"]) == 1) { 
+					?>
+				<th>
+				<th class="column1666 navigation-button"><a href="dodavanje_zivotinje.php">Dodaj novu životinju</a></th>
+				<th class="column1666 navigation-button"><a href="moderator_stranica.php">Moderator stranica</a></th>
+				<th class="column1666 navigation-button">
+					<?php } ?>
+					<?php
+					if(isset($_SESSION["tip"]) && $_SESSION["tip"] == 0) { ?>
+				<th class="column1666 navigation-button"><a href="dodavanje_zivotinje.php">Dodaj novu životinju</a></th>
+				<th class="column1666 navigation-button"><a href="korisnici.php">Korisnici</a></th>
+				<th class="column1666 navigation-button"><a href="registracija.php">Registracija</a></th>
+				<?php } ?>
+			</tr>
+		</table>
+	</div>	
+	<?php } ?>
 </nav>
-</div>
