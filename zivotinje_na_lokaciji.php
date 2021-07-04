8b4946f3640c8c808bffebf5a5836b353755a815
Darko <darkodidak1@gmail.com>
2021-07-04 23:50:20 +0200

    refactor small

--- zivotinje_na_lokaciji.php
+++ zivotinje_na_lokaciji.php
@@ -21,7 +21,7 @@
             <td>{$zivotinja['lokacija_id']}</td>";
             if ($zivotinja["admin"] == 0) {
                 echo "<td><a style=height:20px; width:20px; background-colro:red; 
-                href=\"obrisi_zivotinju.php?lokacija_id=".$id_lokacije."&zivotinja_id=".$zivotinja["zivotinja_id"]."\">Obrisi</a></td>";
+                href=\"upit_obrisi_zivotinju.php?lokacija_id=".$id_lokacije."&zivotinja_id=".$zivotinja["zivotinja_id"]."\">Obrisi</a></td>";
             }
         echo "<td><a href=\"uredi_zivotinju.php?&zivotinja_id=".$zivotinja["zivotinja_id"]."&lokacija_id=".$id_lokacije."\">Uredi</a></td>";
         echo "</tr></table>";
